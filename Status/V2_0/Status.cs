d14145b2a7cb1ad8cd4fe4f3749f9829f904c7e8
robin dunlop <robin@ncvib.com>
2025-06-24 13:33:47 +0200

    Instrument status schema

---
+++ Status/V2_0/Status.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NcvibJson.Status.V2_0;
+
+public class Status
+{
+    [JsonPropertyName("formatVersion")] public required string FormatVersion { get; set; } = "2.0";
+    [JsonPropertyName("batteryLevelVoltage")] public double? BatteryLevelVoltage { get; set; }
+    [JsonPropertyName("batteryLevelPercentage")] public double? BatteryLevelPercentage { get; set; }
+    [JsonPropertyName("signalStrength")] public double? SignalStrength { get; set; }
+    [JsonPropertyName("availableMemory")] public double? AvailableMemory { get; set; }
+    [JsonPropertyName("statusTimeInUtc")] public DateTimeOffset? StatusTimeInUtc { get; set; }
+    [JsonPropertyName("latestDataDeliveryInUtc")] public DateTimeOffset? LatestDataDeliveryInUtc { get; set; }
+    [JsonPropertyName("monitoringActive")] public bool? MonitoringActive { get; set; }
+    
+    [JsonPropertyName("position")] public Coordinates? Position { get; set; }
+    [JsonPropertyName("nodeStatuses")] public NodeStatus[] NodeStatuses { get; set; } = [];
+    
+    public class Coordinates
+    {
+        [JsonPropertyName("longitude")] public double Longitude { get; set; }
+        [JsonPropertyName("latitude")] public double Latitude { get; set; }
+        [JsonPropertyName("elevationInMeters")] public double? ElevationInMeters { get; set; }
+    }
+    
+    public class NodeStatus
+    {
+        [JsonPropertyName("axis")] public string? Axis { get; set; }
+        [JsonPropertyName("active")] public bool? Active { get; set; }
+    }        
+}
