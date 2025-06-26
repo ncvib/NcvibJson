4446b09452ba560562bff339b1efc66f6e47af3a
robin dunlop <robin@ncvib.com>
2025-06-26 13:40:00 +0200

    Validation issues - troubleshooting

--- Common/Definitions/V2_0/Axis.cs
+++ Common/Definitions/V2_0/Axis.cs
@@ -1,7 +1,9 @@
 using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
 
 namespace NcvibJson.Common.Definitions.V2_0;
 
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum Axis
 {
     [EnumMember(Value = "V")] Vertical,
