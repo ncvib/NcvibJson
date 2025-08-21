fc9d27ab899c57e505d5cfef6f4df382e1240344
robin dunlop <robin@ncvib.com>
2025-08-21 13:19:16 +0200

    Troubleshooting a deserialization issue

--- NcvibJson/Common/Standards/V2_0/StandardFilter.cs
+++ NcvibJson/Common/Standards/V2_0/StandardFilter.cs
@@ -5,11 +5,6 @@ namespace NcvibJson.Common.Standards.V2_0;
 [JsonConverter(typeof(StandardFilterJsonConverter))]
 public class StandardFilter
 {
-    [JsonPropertyName("name")] public required string Name { get; set; }
-    [JsonPropertyName("filterDefinition")] public required FilterDefinition FilterDefinition { get; set; }
-    
-    // Empty constructor for deserialization
-    public StandardFilter()
-    {
-    }
+    [JsonPropertyName("name")] public required string Name { get; set; } = "Unknown";
+    [JsonPropertyName("filterDefinition")] public required FilterDefinition FilterDefinition { get; set; } = new() {LowPass = 0, HighPass = 1000};
 }
