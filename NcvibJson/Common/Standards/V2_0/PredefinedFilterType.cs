b7bc5d134f74ab311cdae8295fb42f0e7a417ce8
robin dunlop <robin@ncvib.com>
2025-08-29 10:55:03 +0200

    Strict numbers

--- NcvibJson/Common/Standards/V2_0/PredefinedFilterType.cs
+++ NcvibJson/Common/Standards/V2_0/PredefinedFilterType.cs
@@ -1,23 +1,24 @@
 namespace NcvibJson.Common.Standards.V2_0;
 
+// number should correspond to StandardFilterId  
 public enum PredefinedFilterType
 {
     NoFilter = 0,
-    ISEESeismograph,
-    DIN41503,
-    DIN41502KB,
-    BS7385,
-    AS21872_2006,
-    NORMS9012,
-    ISO8569Accel,
-    IN1226,
-    NS8176Komfort,
-    NS8141Byggverk,
-    NS8141_1_2012_A1_2013,
-    SS4604866Sprang,
-    SS025211Schakt,
-    SS4604861Komfort,
-    Geophone,
-    ICPECirculaire86,
-    SS25211Schakt5_150Hz
+    ISEESeismograph = 1,
+    DIN41503 = 2,
+    DIN41502KB = 3,
+    BS7385 = 4,
+    AS21872_2006 = 5,
+    NORMS9012 = 6,
+    ISO8569Accel = 7,
+    IN1226 = 8,
+    NS8176Komfort = 9,
+    NS8141Byggverk = 10,
+    NS8141_1_2012_A1_2013 = 11,
+    SS4604866Sprang = 12,
+    SS025211Schakt = 13,
+    SS4604861Komfort = 14,
+    Geophone = 15,
+    ICPECirculaire86 = 16,
+    SS25211Schakt5_150Hz = 17
 }
